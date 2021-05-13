cfa45e47ad821d0ff266d7f6cda7826cc4c108e3
Harithya Wisesa <harithya77@gmail.com>
2021-05-13 09:17:48 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,6 +24,7 @@ export class Home extends Component {
             .then(res => {
                 this.setState({ 'popular': res.data.manga_list })
             })
+        console.log(this.state.popular);
     }
 
     render() {
